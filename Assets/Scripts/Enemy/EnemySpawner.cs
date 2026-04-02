4476efce7ea481684df7eafa2301249d5e6d4403
n0wst4ndup <dlwotjtj9898@gmail.com>
2026-04-02 20:09:10 +0900

    Feat: Spawner 구현

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -6,5 +6,29 @@ public class EnemySpawner : MonoBehaviour
     public EnemyBase[] Prefabs;
     public Transform[] SpawnPoints;
 
+    public int EnemyCapacity = 30;
+
+    private float _spawnInterval = 3f;
+    private float _timer = 3f;
+
     private List<EnemyBase> _enemies = new();
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_spawnInterval > _timer || _enemies.Count == EnemyCapacity) return;
+
+        CreateEnemy();
+
+        _timer = 0;
+    }
+
+    private void CreateEnemy()
+    {
+        var point = SpawnPoints[Random.Range(0, SpawnPoints.Length)];
+        var enemy = Instantiate(Prefabs[Random.Range(0, Prefabs.Length)], point.position, point.rotation);
+        _enemies.Add(enemy);
+
+        enemy.GetComponent<LivingEntity>().OnDead.AddListener(() => _enemies.Remove(enemy));
+    }
 }
